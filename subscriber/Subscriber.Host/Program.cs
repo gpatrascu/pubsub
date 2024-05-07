72e3267200badd04d209a5761701096894592d93
George-Patrascu <g.patrascu@criteo.com>
2024-05-07 15:04:14 +0300

    Add subscriber - a simple console that use the first argument as the subscriber name.

---
+++ subscriber/Subscriber.Host/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Net.Http.Json;
+using PubSub.Domain;
+
+string subscriberName = Guid.NewGuid().ToString();
+if (args.Length > 0)
+{
+    subscriberName = args[0];
+}
+var httpClient = new HttpClient
+{
+    BaseAddress = new Uri("http://localhost:5117")
+};
+
+string topicName = "order-submitted";
+
+ConsumeMessages().Wait();
+
+async Task ConsumeMessages()
+{
+    var stream = httpClient.GetFromJsonAsAsyncEnumerable<PubSubMessage>(
+        $"topics/{topicName}/subscriptions/{subscriberName}/messages");
+
+    await foreach (var message in stream)
+    {
+        // here we can deserialize the message and base on it send a command to an
+        // appropriate handler that exists in some hypothetical application service 
+        Console.WriteLine($"Received message: {message.Payload}");
+    }
+}
+
+
+
