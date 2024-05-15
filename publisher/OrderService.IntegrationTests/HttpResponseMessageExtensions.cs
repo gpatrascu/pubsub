695cf3e4503c271f49f6479eecedac74478ec729
George-Patrascu <g.patrascu@criteo.com>
2024-05-15 23:18:13 +0300

    refactor

--- publisher/OrderService.IntegrationTests/HttpResponseMessageExtensions.cs
+++ publisher/OrderService.IntegrationTests/HttpResponseMessageExtensions.cs
@@ -4,10 +4,13 @@ namespace OrderService.IntegrationTests;
 
 public static class HttpResponseMessageExtensions
 {
+    private static readonly JsonSerializerOptions JsonSerializerOptions =
+        new() { PropertyNameCaseInsensitive = true };
+
     public static async Task<T> ReadAsJson<T>(this HttpResponseMessage httpResponseMessage)
     {
         var content = await httpResponseMessage.Content.ReadAsStringAsync();
-        return JsonSerializer.Deserialize<T>(content, 
-            new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+        return JsonSerializer.Deserialize<T>(content,
+            JsonSerializerOptions);
     }
 }
